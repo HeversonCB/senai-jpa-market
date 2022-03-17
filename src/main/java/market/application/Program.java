29957c24ceb786c0a59ee25ced66e7de840bc34b
HeversonCB <heversonbenatti@hotmail.com>
2022-03-16 21:04:51 -0300

    Adicionado Log4j na aplicação e configuração do Log

--- src/main/java/market/application/Program.java
+++ src/main/java/market/application/Program.java
@@ -1,10 +1,17 @@
 package market.application;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Program {
 
+	private static final Logger LOG = LogManager.getLogger(Program.class);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		LOG.info("Hello World Log4j");
+		
+		
 	}
 
 }
